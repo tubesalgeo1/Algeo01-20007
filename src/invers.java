590e90d3ffa6eb0285f854d1f6b4dabd24626ede
Nadia Mareta <82803405+KorbanFidas2A@users.noreply.github.com>
2021-09-26 19:49:51 +0700

    Create invers.java

---
+++ src/invers.java
@@ -0,0 +1,26 @@
+public class invers{
+    /* mencari matrix balikan dengan
+    1/det*adjoint */
+    public static float[][] invers_mat(float a[][]){
+        float determinan;
+        int size = a.length;
+        float adjoint[][] = Determinan.adjoin(a);
+        float array[][] = new float[size][size];
+        /* isi dulu */
+        determinan = Determinan.detReduksiBaris(a);
+        array = multiply_matrix(adjoint, 1/determinan);
+        return array;
+    }
+
+    public static float[][] multiply_matrix(float a[][], float var){
+        int size = a.length;
+        float array[][] = new float[size][size];
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                array[i][j] = a[i][j] * var;
+            }
+        }
+
+        return array;
+    }
+}
