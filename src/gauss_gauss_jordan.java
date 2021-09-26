15fa5025a1f89a5c34cb4d427ca1bcf9ea40c909
Nadia Mareta <82803405+KorbanFidas2A@users.noreply.github.com>
2021-09-26 13:27:28 +0700

    Update gauss_gauss_jordan.java

--- src/gauss_gauss_jordan.java
+++ src/gauss_gauss_jordan.java
@@ -1,9 +1,8 @@
 public class gauss_gauss_jordan{
-
     /* Note buat kawan2 jadi ini pertama harus lewat 
         1. panggil fungsi elimination_before terlebih dahulu
         (ini untuk menghandle swap baris)
-        NOTE: JANGAN LUPA UNTUK INISIALISASI VARIABEL BUAT MENAMPUNG NILAI SWAP YAK KARENA BAKAL KEPAKE BANGET DI DETERMINAN LAGI 
+        NOTE: JANGAN LUPA UNTUK INISIALISASI VARIABEL BUAT SWAP DALAM BENTUK ARRAY PANJANGNYA 1 DIGIT YAK KARENA BAKAL KEPAKE BANGET DI DETERMINAN LAGI 
         2. panggil fungsi gauss()
         3. panggil fungsi gauss_jordan() apabila berniat untuk mengubah ke bentuk matriks baris tereduksi -> opsional
         4. Selesai */
@@ -18,16 +17,17 @@ public class gauss_gauss_jordan{
         }
     }
 
-    public static void check_gauss(float m[][], int neffrow, int neffcols, int swap_counter){
+    public static void check_gauss(float m[][], int neffrow, int neffcols, int swap_counter[]){
         int h, i, j;
         int n;
+
         for(h = 0; h < neffrow; h++){
             for(i = 1; i < neffrow; i++){
                 for(j = 0; j < neffcols; j++){
                     if(m[i][j] != 0 && (m[i-1][j] == 0)){
                         n = i - 1;
                         swap(m, i, n, neffrow, neffcols);
-                        swap_counter ++;
+                        swap_counter[0] += 1;
                         break;
                     }
                 }
@@ -108,23 +108,40 @@ public class gauss_gauss_jordan{
         return index;
     }
 
-    public static void elimination_before(float m[][], int neffrow, int neffcols, int swap_counter){
+    public static void elimination_before(float m[][], int neffrow, int neffcols, int swap_counter[]){
         check_gauss(m, neffrow, neffcols, swap_counter);
         float idx1, idx2;
         int i, j, k;
         for(i = 1; i < neffrow; i++){
             for(j = 0; j < i; j++){
                 if(m[j][j] == 0){
-                    check_gauss(m, neffrow, neffcols, swap_counter);
-                    i = 1;
-                    break;
-                }
-                idx1 = m[i][j];
-                idx2 = m[j][j];
-                for(k = 0; k < neffcols; k++){
-                    m[i][k] -= (idx1*m[j][k])/idx2;
+                    if(is_singular(m, j, i, neffcols)){
+                        /* do nothing */
+                    }else{
+                        check_gauss(m, neffrow, neffcols, swap_counter);
+                        i = 1;
+                        break;
+                    }
+                }else{
+                    idx1 = m[i][j];
+                    idx2 = m[j][j];
+                    for(k = 0; k < neffcols; k++){
+                        m[i][k] -= (idx1*m[j][k])/idx2;
+                    }
                 }
             }
         }
     }
+
+    public static boolean is_singular(float m[][], int j, int i, int neffcols){
+        boolean flag;
+        flag = true;
+        for(int k = 0; k < neffcols; k++){
+            if(m[i][k] != 0 || m[j][k] != 0){
+                flag = false;
+            }
+        }
+        return flag;
+    }
+
 }
