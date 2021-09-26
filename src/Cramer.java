d45cdb6eedc4c661fda5688bfd795fe7a69f1890
HanselTanoto <hansel.tanoto05@gmail.com>
2021-09-26 10:41:57 +0700

    Update determinan dan cramer

--- src/Cramer.java
+++ src/Cramer.java
@@ -1,19 +1,67 @@
-public class Cramer {
+// import java.io.IOException;
 
-    public static float[][] insertConst(float[][] matriks, int j, float[] constant) {
-        int rows = matriks.length;
-        for (int i = 0; i < rows; i++) {
-            matriks[i][j] = constant[i];
+public class Cramer {
+    /* // TEST 
+    public static void main(String[] args) throws IOException {
+        // Prosedur main untuk mengetes fungsi pada class Cramer
+        float[][] mAugmented = ReadDisplayArray.readInput(false);
+        ReadDisplayArray.displayOutput(mAugmented);
+        Cramer.printSol(cramerSol(mAugmented));
+    } */
+
+    public static void printSol(float[] solution) {
+        // Prosedur printSol mencetak nilai pada array solution satu per satu
+        for (int i = 0; i < solution.length; i++) {
+            System.out.println("x" + i + " = " + solution[i]);
         }
-        return matriks; 
+    }
+
+    private static float[][] insertConst(float[][] matriks, int j, float[] constant) {
+        // Fungsi insertConst mengganti elemen pada kolom j dengan nilai array constant (ruas kanan SPL)
+        // Copy matriks ke matriks result
+        int rows = matriks.length;
+        int cols = matriks[0].length;
+        float [][] result = new float[rows][cols];
+        for (int l = 0; l < rows; l++) {
+            for (int k = 0; k < cols; k++) {
+                result[l][k] = matriks[l][k];
+            }
+        }
+        // Mengganti kolom j dengan nilai pada array constant
+        for (int i = 0; i < rows; i++) {
+            result[i][j] = constant[i];
+        }
+        return result; 
     }
     
-    public static float[] crammerSol(float[][] matriks, float[] constant) {
-        int rows = matriks.length;
-        float[] solution = new float[rows];
-        float detKoef = Determinan.detKofaktor(matriks);
+    public static float[] cramerSol(float[][] mAugmented) {
+        // Fungsi cramerSol menghasilkan solusi SPL dengan menggunakan kaidah Cramer
+        // Dimensi matriks Augmented (mAugmented)
+        int rows = mAugmented.length;
+        int cols = mAugmented[0].length;
+
+        // Copy koefisien variabel x1, x2, ..., xn (ruas kiri SPL) ke matriks baru (mKoef)
+        float[][] mKoef = new float[rows][cols-1];
         for (int i = 0; i < rows; i++) {
-            solution[i] = (Determinan.detKofaktor(insertConst(matriks, i, constant))) / detKoef;
+            for (int j = 0; j < cols-1; j++) {
+                mKoef[i][j] = mAugmented[i][j];
+            }
+        } 
+        // Menghitung determinan matriks koefisien
+        float detKoef = Determinan.detKofaktor(mKoef);
+        
+        // Copy konstanta (ruas kanan SPL) ke array baru (constant)
+        float[] constant = new float[rows];
+        for (int i = 0; i < rows; i++) {
+            constant[i] = mAugmented[i][cols-1];
+        } 
+
+        // Deklarasi array untuk menampung nilai dari solusi (x1, x2, ..., xn)
+        float[] solution = new float[rows];
+        // Menghitung solusi dan memasukkan nilainya ke array solusi (solution)
+        for (int j = 0; j < cols-1; j++) {
+            float[][] temp = mKoef;
+            solution[j] = (Determinan.detKofaktor(insertConst(temp, j, constant))) / detKoef;
         }
         return solution;
     }
--- src/Determinan.java
+++ src/Determinan.java
@@ -1,9 +1,10 @@
-import java.io.IOException;
+//import java.io.IOException;
 import java.lang.Math;
 
 public class Determinan {
     /* // Test 
     public static void main(String[] args) throws IOException {
+        // Prosedur main untuk mengetes fungsi pada class Determinan
         float[][] matriks = ReadDisplayArray.readInput(true);
         ReadDisplayArray.displayOutput(matriks);
         System.out.println("Determinan = " + Determinan.detKofaktor(matriks));
@@ -11,16 +12,19 @@ public class Determinan {
     }*/
 
     public static float[][] minor (float[][] matriks, int i, int j) {
+        // Fungsi minor menghasilkan matriks minor (Mij) dari suatu matriks
+        // Deklarasi matriks result
         int size = matriks.length-1;
         float[][] result = new float[size][size];
         int c = 0; 
+        // Looping untuk menghasilkan matriks minor
         for (int a = 0; a < matriks.length; a++) {
-            if (a == i) {
+            if (a == i) {   // Skip baris i
                 continue;
             }
             int d = 0;
             for (int b = 0; b < matriks.length; b++) {
-                if (b == j) {
+                if (b == j) {   // Skip kolom j
                     continue;
                 }
                 result[c][d] = matriks[a][b];
@@ -32,13 +36,14 @@ public class Determinan {
     }
     
     public static float detKofaktor (float[][] matriks) {
+        // FUngsi detKofaktor menghasilkan determian matriks dengan metode ekspansi kofaktor
         int size = matriks[0].length;
         float result = 0;
-        if (size == 1) {
+        if (size == 1) {            // matriks dengan ukuran 1x1
             return matriks[0][0];
-        } else if (size == 2) {
+        } else if (size == 2) {     // matriks dengan ukuran 2x2
             return (matriks[0][0] * matriks[1][1]) - (matriks[1][0] * matriks[0][1]);
-        } else {
+        } else {                    // matriks dengan ukuran > 2x2
             int j = 0;
             for (int i = 0; i < size; i++) {
                 result += matriks[i][j] * detKofaktor(minor(matriks, i, j)) * Math.pow(-1, i+j);
@@ -48,14 +53,15 @@ public class Determinan {
     }
 
     public static float detReduksiBaris (float[][] matriks) {
+        // FUngsi detKofaktor menghasilkan determian matriks dengan metode reduksi baris
         int swap = 0;
         int size = matriks.length;
-        if (size == 1) {
+        if (size == 1) {        // matriks dengan ukuran 1x1
             return matriks[0][0];
-        } else {
+        } else {                // matriks dengan ukuran > 1x1
             float det = 1;
-            gauss_gauss_jordan.elimination_before(matriks, size, size, swap);
-            for (int i = 0; i < size; i++) {
+            gauss_gauss_jordan.elimination_before(matriks, size, size, swap);   // menerapkan OBE untuk membentuk matriks eselon
+            for (int i = 0; i < size; i++) {    // Mengalikan semua elemen pada diagonal utama
                 det = det * matriks[i][i];
             }
             return det * (float) Math.pow(-1, swap);
