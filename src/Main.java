3bc0025d4e14e3b6a225cc6706d20b3a6e0c56c6
KorbanFidas2A <nadia.leiden6@gmail.com>
2021-09-29 23:01:17 +0700

    change main

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,38 @@ public class Main {
             // Sistem Persamaan Linier
             if (choiceMenu == 1) {
                 System.out.println("******************** SISTEM PERSAMAAN LINIER ********************");
+                float matrix1[][] = ReadDisplayArray.readInput(false);
+                int rows = matrix1.length;
+                int cols = matrix1[0].length;
+                int arr[] = new int[1];
+                arr[0] = 0;
+                System.out.println();
+                gauss_gauss_jordan.elimination_before(matrix1, rows, cols, arr);
+                System.out.println("Eliminasi sebelum gauss (matriks segitiga bawah): ");
+                for(int i = 0; i < rows; i++){
+                    for(int j = 0; j < cols; j++){
+                        System.out.print(matrix1[i][j] + " ");
+                    }
+                    System.out.println();
+                }
+                System.out.println();
+                System.out.println("Eliminasi Gauss: ");
+                gauss_gauss_jordan.gauss(matrix1, rows, cols);
+                for(int i = 0; i < rows; i++){
+                    for(int j = 0; j < cols; j++){
+                        System.out.print(matrix1[i][j] + " ");
+                    }
+                    System.out.println();
+                }
+                System.out.println();
+                System.out.println("Eliminasi Gauss Jordan: ");
+                gauss_gauss_jordan.gauss_jordan(matrix1, rows, cols);
+                for(int i = 0; i < rows; i++){
+                    for(int j = 0; j < cols; j++){
+                        System.out.print(matrix1[i][j] + " ");
+                    }
+                    System.out.println();
+                }
             }
 
             // Determinan
@@ -23,8 +55,8 @@ public class Main {
             // Matriks Balikan
             else if (choiceMenu == 3) {
                 System.out.println("******************** MATRIKS BALIKAN ********************");
-                float matrix[][] = ReadDisplayArray.readInputPoint();
-                matrix = invers.invers_mat(matrix);
+                float matrix[][] = ReadDisplayArray.readInput(true);
+                matrix = invers.invers_mat_kofaktor(matrix);
                 System.out.println();
                 for(int i = 0; i < matrix.length; i++){
                     for(int j = 0; j < matrix.length; j++){
