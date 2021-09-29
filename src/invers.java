150d313f01357e40484b1b87357d370d6e4df3fb
KorbanFidas2A <nadia.leiden6@gmail.com>
2021-09-29 22:01:21 +0700

    name change

--- src/invers.java
+++ src/invers.java
@@ -1,18 +1,18 @@
 public class invers{
     /* mencari matrix balikan dengan
     1/det*adjoint */
-    public static float[][] invers_mat(float a[][]){
+    public static float[][] invers_mat_kofaktor(float a[][]){
         float determinan;
         int size = a.length;
         float adjoint[][] = Determinan.adjoin(a);
         float array[][] = new float[size][size];
         /* isi dulu */
         determinan = Determinan.detKofaktor(a);
-        array = multiply_matrix(adjoint, 1/determinan);
+        array = multiply_matrix_kofaktor(adjoint, 1/determinan);
         return array;
     }
 
-    public static float[][] multiply_matrix(float a[][], float var){
+    public static float[][] multiply_matrix_kofaktor(float a[][], float var){
         int size = a.length;
         float array[][] = new float[size][size];
         for(int i = 0; i < size; i++){
@@ -23,5 +23,29 @@ public class invers{
 
         return array;
     }
+
+    public static float[][] invers_mat_reduc(float a[][]){
+        float determinan;
+        int size = a.length;
+        float adjoint[][] = Determinan.adjoin(a);
+        float array[][] = new float[size][size];
+        /* isi dulu */
+        determinan = Determinan.detReduksiBaris(a);
+        array = multiply_matrix_reduc(adjoint, 1/determinan);
+        return array;
+    }
+
+    public static float[][] multiply_matrix_reduc(float a[][], float var){
+        int size = a.length;
+        float array[][] = new float[size][size];
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                array[i][j] = a[i][j] * var;
+            }
+        }
+
+        return array;
+    }
+    
 }
 
