6713878421b2d4d0501d6c2dc8a8911fd1f68ecb
HanselTanoto <hansel.tanoto05@gmail.com>
2021-10-01 07:41:46 +0700

    nambah paramter swap_counter di regresi linear

--- src/RegresiLinier.java
+++ src/RegresiLinier.java
@@ -56,7 +56,7 @@ public class RegresiLinier {
 
         gauss_gauss_jordan.elimination_before(matrix_final, rows, cols, swap_counter);
         gauss_gauss_jordan.gauss(matrix_final, rows, cols);
-        gauss_gauss_jordan.gauss_jordan(matrix_final, rows, cols);
+        gauss_gauss_jordan.gauss_jordan(matrix_final, rows, cols, swap_counter);
 
         String SPL = SPLInterpolasiRegresi(matrix_final, true);
         float result = 0;
@@ -153,7 +153,7 @@ public class RegresiLinier {
         int swap[] = new int[1];
         gauss_gauss_jordan.elimination_before(matrix_final, matrix_final.length, matrix_final[0].length, swap);
         gauss_gauss_jordan.gauss(matrix_final, matrix_final.length, matrix_final[0].length);
-        gauss_gauss_jordan.gauss_jordan(matrix_final, matrix_final.length, matrix_final[0].length);
+        gauss_gauss_jordan.gauss_jordan(matrix_final, matrix_final.length, matrix_final[0].length, swap);
 
         String SPL = SPLInterpolasiRegresi(matrix_final, false);
         String resultString;
